d8caabe085d091e36883e070cc3e5b91c3301e59
bea-codes <bia906@gmail.com>
2022-11-07 20:08:30 -0300

    commit acessibilidade + js

---
+++ contato-script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById('form');
+const nome = document.getElementById('nome-contato-form');
+const email = document.getElementById('email-contato-form');
+const assunto = document.getElementById('assunto-contato-form');
+const mensagem = document.getElementById('mensagem-contato-form');
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+})
+
+
